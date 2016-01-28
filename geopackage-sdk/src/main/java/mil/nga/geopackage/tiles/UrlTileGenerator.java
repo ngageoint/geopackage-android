8ae3ef2edc4d8d996f9040112164a90a14927edc
Brian Osborn <bosborn@users.noreply.github.com>
2016-01-28 08:00:31 -0700

    Tile generation pre tile generation method with feature tile generator table linking of feature and tile tables

--- geopackage-sdk/src/main/java/mil/nga/geopackage/tiles/UrlTileGenerator.java
+++ geopackage-sdk/src/main/java/mil/nga/geopackage/tiles/UrlTileGenerator.java
@@ -219,6 +219,14 @@ public class UrlTileGenerator extends TileGenerator {
      * {@inheritDoc}
      */
     @Override
+    protected void preTileGeneration() {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     protected byte[] createTile(int z, long x, long y) {
 
         byte[] bytes = null;
