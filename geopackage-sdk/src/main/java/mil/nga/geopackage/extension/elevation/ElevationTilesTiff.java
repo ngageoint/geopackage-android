cb510af366a531d2ec1dfb702ad777db44b16fee
Brian Osborn <bosborn@users.noreply.github.com>
2017-10-25 07:15:54 -0600

    change samples per pixel to expect int

--- geopackage-sdk/src/main/java/mil/nga/geopackage/extension/elevation/ElevationTilesTiff.java
+++ geopackage-sdk/src/main/java/mil/nga/geopackage/extension/elevation/ElevationTilesTiff.java
@@ -155,7 +155,7 @@ public class ElevationTilesTiff extends ElevationTilesCommon<ElevationTiffImage>
             throw new GeoPackageException("The image is null");
         }
 
-        Integer samplesPerPixel = directory.getSamplesPerPixel();
+        int samplesPerPixel = directory.getSamplesPerPixel();
         Integer bitsPerSample = null;
         if (directory.getBitsPerSample() != null && !directory.getBitsPerSample().isEmpty()) {
             bitsPerSample = directory.getBitsPerSample().get(0);
@@ -165,7 +165,7 @@ public class ElevationTilesTiff extends ElevationTilesCommon<ElevationTiffImage>
             sampleFormat = directory.getSampleFormat().get(0);
         }
 
-        if (samplesPerPixel == null || samplesPerPixel != SAMPLES_PER_PIXEL
+        if (samplesPerPixel != SAMPLES_PER_PIXEL
                 || bitsPerSample == null || bitsPerSample != BITS_PER_SAMPLE
                 || sampleFormat == null || sampleFormat != TiffConstants.SAMPLE_FORMAT_FLOAT) {
             throw new GeoPackageException(
