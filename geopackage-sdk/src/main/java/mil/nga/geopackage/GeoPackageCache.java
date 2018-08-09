bca4acb8236abba849fe2915db57bb74fae14c15
Brian Osborn <bosborn@users.noreply.github.com>
2018-08-09 14:30:35 -0600

    cache open with writable flag option

--- geopackage-sdk/src/main/java/mil/nga/geopackage/GeoPackageCache.java
+++ geopackage-sdk/src/main/java/mil/nga/geopackage/GeoPackageCache.java
@@ -28,9 +28,21 @@ public class GeoPackageCache extends GeoPackageCoreCache<GeoPackage> {
      * @return GeoPackage
      */
     public GeoPackage getOrOpen(String name) {
+        return getOrOpen(name, true);
+    }
+
+    /**
+     * Get the cached GeoPackage or open and cache the GeoPackage
+     *
+     * @param name     GeoPackage name
+     * @param writable writable true to open as writable, false as read only
+     * @return GeoPackage
+     * @since 3.0.3
+     */
+    public GeoPackage getOrOpen(String name, boolean writable) {
         GeoPackage geoPackage = get(name);
         if (geoPackage == null) {
-            geoPackage = manager.open(name);
+            geoPackage = manager.open(name, writable);
             add(geoPackage);
         }
         return geoPackage;
