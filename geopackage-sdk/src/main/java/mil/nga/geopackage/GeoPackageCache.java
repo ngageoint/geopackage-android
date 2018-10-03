cb0265828c7e8dba49511d76f0e8d4a024c27dd1
Brian Osborn <bosborn@users.noreply.github.com>
2018-10-03 10:13:00 -0600

    get or open without caching options

--- geopackage-sdk/src/main/java/mil/nga/geopackage/GeoPackageCache.java
+++ geopackage-sdk/src/main/java/mil/nga/geopackage/GeoPackageCache.java
@@ -40,10 +40,47 @@ public class GeoPackageCache extends GeoPackageCoreCache<GeoPackage> {
      * @since 3.1.0
      */
     public GeoPackage getOrOpen(String name, boolean writable) {
+        return getOrOpen(name, writable, true);
+    }
+
+    /**
+     * Get the cached GeoPackage or open the GeoPackage file without caching it
+     *
+     * @param name GeoPackage name
+     * @return GeoPackage
+     * @since 3.1.0
+     */
+    public GeoPackage getOrNoCacheOpen(String name) {
+        return getOrNoCacheOpen(name, true);
+    }
+
+    /**
+     * Get the cached GeoPackage or open the GeoPackage file without caching it
+     *
+     * @param name     GeoPackage name
+     * @param writable writable true to open as writable, false as read only
+     * @return GeoPackage
+     * @since 3.1.0
+     */
+    public GeoPackage getOrNoCacheOpen(String name, boolean writable) {
+        return getOrOpen(name, writable, false);
+    }
+
+    /**
+     * Get the cached GeoPackage or open and cache the GeoPackage
+     *
+     * @param name     GeoPackage name
+     * @param writable writable true to open as writable, false as read only
+     * @param cache    true to cache opened GeoPackages
+     * @return GeoPackage
+     */
+    private GeoPackage getOrOpen(String name, boolean writable, boolean cache) {
         GeoPackage geoPackage = get(name);
         if (geoPackage == null) {
             geoPackage = manager.open(name, writable);
-            add(geoPackage);
+            if (cache) {
+                add(geoPackage);
+            }
         }
         return geoPackage;
     }
