d11f0aa053aad096363d408bdc19393c73f13cfc
Brian Osborn <bosborn@users.noreply.github.com>
2019-03-08 14:41:20 -0700

    example tile scaling update

--- geopackage-sdk/src/androidTest/java/mil/nga/geopackage/test/GeoPackageExample.java
+++ geopackage-sdk/src/androidTest/java/mil/nga/geopackage/test/GeoPackageExample.java
@@ -1931,9 +1931,17 @@ public class GeoPackageExample extends BaseTestCase {
             TileTableScaling tileTableScaling = new TileTableScaling(
                     geoPackage, tileTable);
             TileScaling tileScaling = new TileScaling();
-            tileScaling.setScalingType(TileScalingType.IN_OUT);
             tileScaling.setZoomIn(2l);
-            tileScaling.setZoomOut(2l);
+            FeatureTileTableLinker linker = new FeatureTileTableLinker(
+                    geoPackage);
+            if (linker.has()
+                    && !linker.getFeatureTablesForTileTable(tileTable)
+                    .isEmpty()) {
+                tileScaling.setScalingType(TileScalingType.IN);
+            } else {
+                tileScaling.setScalingType(TileScalingType.IN_OUT);
+                tileScaling.setZoomOut(2l);
+            }
             tileTableScaling.create(tileScaling);
 
         }
