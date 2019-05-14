0f404e392e1aa6d2624b1da2a3ade93c665d53f1
Brian Osborn <bosborn@gmail.com>
2019-05-14 11:39:52 -0600

    Android Q fix

--- geopackage-sdk/src/main/java/mil/nga/geopackage/db/SQLUtils.java
+++ geopackage-sdk/src/main/java/mil/nga/geopackage/db/SQLUtils.java
@@ -1,10 +1,10 @@
 package mil.nga.geopackage.db;
 
 import android.content.ContentValues;
-import android.os.Parcel;
 
-import java.util.HashMap;
 import java.util.Map;
+
+import mil.nga.geopackage.GeoPackageException;
 
 /**
  * Core SQL Utility methods
@@ -24,19 +24,51 @@ public class SQLUtils {
         ContentValues quoteValues = null;
         if (values != null) {
 
-            Map<String, Object> quoteMap = new HashMap<>();
+            quoteValues = new ContentValues();
+
             for (Map.Entry<String, Object> value : values.valueSet()) {
-                quoteMap.put(CoreSQLUtils.quoteWrap(value.getKey()), value.getValue());
+                setContentValue(quoteValues, CoreSQLUtils.quoteWrap(value.getKey()), value.getValue());
             }
 
-            Parcel parcel = Parcel.obtain();
-            parcel.writeMap(quoteMap);
-            parcel.setDataPosition(0);
-            quoteValues = ContentValues.CREATOR.createFromParcel(parcel);
-            parcel.recycle();
         }
 
         return quoteValues;
+    }
+
+    /**
+     * Set the content value into the content values
+     *
+     * @param contentValues content values
+     * @param key           key
+     * @param value         object value
+     * @since 3.2.1
+     */
+    public static void setContentValue(ContentValues contentValues, String key, Object value) {
+
+        if (value == null) {
+            contentValues.putNull(key);
+        } else if (value instanceof String) {
+            contentValues.put(key, (String) value);
+        } else if (value instanceof Long) {
+            contentValues.put(key, (Long) value);
+        } else if (value instanceof Integer) {
+            contentValues.put(key, (Integer) value);
+        } else if (value instanceof Short) {
+            contentValues.put(key, (Short) value);
+        } else if (value instanceof Byte) {
+            contentValues.put(key, (Byte) value);
+        } else if (value instanceof Double) {
+            contentValues.put(key, (Double) value);
+        } else if (value instanceof Float) {
+            contentValues.put(key, (Float) value);
+        } else if (value instanceof Boolean) {
+            contentValues.put(key, (Boolean) value);
+        } else if (value instanceof byte[]) {
+            contentValues.put(key, (byte[]) value);
+        } else {
+            throw new GeoPackageException("Unsupported Content Values value: " + value + ", key: " + key);
+        }
+
     }
 
 }
