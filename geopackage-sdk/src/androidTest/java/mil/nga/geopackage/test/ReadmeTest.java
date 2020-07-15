134d0ddd1074f4de11b87ef8112530105f95f6ff
Brian Osborn <bosborn@gmail.com>
2020-07-15 05:33:34 -0600

    test update

--- geopackage-sdk/src/androidTest/java/mil/nga/geopackage/test/ReadmeTest.java
+++ geopackage-sdk/src/androidTest/java/mil/nga/geopackage/test/ReadmeTest.java
@@ -178,12 +178,8 @@ public class ReadmeTest extends ImportGeoPackageTestCase {
         int urlTileCount = urlTileGenerator.generateTiles();
 
         // Feature Tile Generator (generate tiles from features)
-        String featureTilesTable = featureTable + "_tiles";
-        if(geoPackage.isTileTable(featureTilesTable)){
-            projection = geoPackage.getTileDao(featureTilesTable).getProjection();
-        }
         TileGenerator featureTileGenerator = new FeatureTileGenerator(context, geoPackage,
-                featureTilesTable, featureTiles, 1, 2, boundingBox, projection);
+                "tiles_" + featureTable, featureTiles, 1, 2, boundingBox, projection);
         int featureTileCount = featureTileGenerator.generateTiles();
 
         // Close feature tiles (and indexer)
